2e3718457fa0812bd3a2e1a92cbcd70840dd6160
kerambit <liukang1488666228@gmail.com>
2025-06-10 10:46:35 +0400

    add sceleton for controller, module, service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UrlController } from './url/url.controller';
+import { UrlModule } from './url/url.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UrlModule],
+  controllers: [AppController, UrlController],
   providers: [AppService],
 })
 export class AppModule {}
